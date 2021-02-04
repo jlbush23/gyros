cb9e52ff051fbe6594f2e5b3c40116d63254bc23
jlbush23 <33840348+jlbush23@users.noreply.github.com>
2021-02-04 16:53:14 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,7 +6,17 @@ Created on Wed Feb  3 21:44:18 2021
 """
 from setuptools import setup
 
+dependencies = ['numpy','pandas','os','shutil','pickle',
+                'astroquery','astropy','exoplanet','matplotlib','scipy']
+
 setup(
-      name = 'NASA_LCs'
-      
+      name = 'NASA_LCs',
+      url = 'https://github.com/jlbush23/NASA_LCs',
+      author = 'Jonathan Bush',
+      author_email = 'jlbush23@gmail.com',
+      packages = ['NASA_LCs'],
+      install_requires = dependencies,
+      version = '0.1',
+      description = "Download light curves from NASA's Kepler, K2, and TESS space missions!",
+      #long_description = open('README.md').read()      
      )
