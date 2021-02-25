21fa817a1ab06984bcf01ae6c8bb3fe1fe2cb243
jlbush23 <jlbush23@gmail.com>
2021-02-25 12:09:12 -0500

    added try/except to bulk_download function

--- NASA_LCs/tools.py
+++ NASA_LCs/tools.py
@@ -37,14 +37,17 @@ def bulk_download(tic_list, download_dir, products = ['LC'], run_rotations = Tru
     
     for i,tic in enumerate(tic_list):
         print("Working on object " + str(i+1) + "/" + str(len(tic_list)) + ".")
-        print(tic)
-        target_obj = mastQuery.mast_target(tic = tic)
-        target_obj.download(products = products, download_dir = download_dir)
-        if run_rotations == True: 
-            target_obj.run_rotation_tools(flux_type = rotation_options['flux_type'],
-                                          flux_err_avail = rotation_options['flux_err_avail'],
-                                          min_freq = rotation_options['min_freq'])
-        if save_objects == True: save_object(target_object = target_obj, keep_fits = keep_fits)
-        
-        del target_obj
+        #print(tic)
+        try: 
+            target_obj = mastQuery.mast_target(tic = tic)
+            target_obj.download(products = products, download_dir = download_dir)
+            if run_rotations == True: 
+                target_obj.run_rotation_tools(flux_type = rotation_options['flux_type'],
+                                              flux_err_avail = rotation_options['flux_err_avail'],
+                                              min_freq = rotation_options['min_freq'])
+            if save_objects == True: save_object(target_object = target_obj, keep_fits = keep_fits)
+            
+            del target_obj
+        except:
+            print("Data download error. Check if SPOC available on MAST.")
         
